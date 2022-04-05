8304424424395e1d44796c5277cc722a5f491728
BenmrChen <mingren.chen16@gmail.com>
2022-04-05 18:28:09 +0800

    set initialSupply in deploy.js for CLToken

--- contracts/CLToken.sol
+++ contracts/CLToken.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CLToken is ERC20 {
-    constructor() ERC20("CLToken", "CLT") {
-        _mint(msg.sender, 100000000 * 10 ** decimals());
+    constructor(uint256 initialSupply) ERC20("CLToken", "CLT") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
 }

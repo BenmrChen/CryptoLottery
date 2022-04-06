36ade4e86fac6e2d9e37f2ecacf9bf87cb7fba73
BenmrChen <mingren.chen16@gmail.com>
2022-04-06 10:37:51 +0800

    revise vendor exchange rate

--- contracts/Vendor.sol
+++ contracts/Vendor.sol
@@ -21,13 +21,16 @@ contract Vendor is Ownable {
     IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
   }
 
+  function tokenSupply() public view returns (uint) {
+    return token.balanceOf(address(this));
+  }
   /**
   * 以 ETH 買 CLT
   */
   function buyTokens() public payable returns (uint256 tokenAmount) {
     require(msg.value > 0, "Send ETH to buy some tokens");
 
-    uint256 amountToBuy = msg.value * tokensPerEth;
+    uint256 amountToBuy = msg.value/1000000000000000000 * tokensPerEth;
 
     // 確認餘額是否足夠
     uint256 vendorBalance = token.balanceOf(address(this));
@@ -54,25 +57,25 @@ contract Vendor is Ownable {
     require(sent, "Failed to send user balance back to the owner");
   }
 
-  // 賣 token 換 ETH
-  function sellTokens(uint256 tokenAmountToSell) public {
+    // 賣 token 換 ETH
+    function sellTokens(uint256 tokenAmountToSell) public {
     // 檢查 user 要賣的 token > 0
-    require(tokenAmountToSell > 0, "Specify an amount of token greater than zero");
+      require(tokenAmountToSell > 0, "Specify an amount of token greater than zero");
 
-    // 檢查使用者餘額足夠
-    uint256 userBalance = token.balanceOf(msg.sender);
-    require(userBalance >= tokenAmountToSell, "Your balance is lower than the amount of tokens you want to sell");
+      // 檢查使用者餘額足夠
+      uint256 userBalance = token.balanceOf(msg.sender);
+      require(userBalance >= tokenAmountToSell, "Your balance is lower than the amount of tokens you want to sell");
 
-    // 檢查合約的餘額是否足夠
-    uint256 amountOfETHToTransfer = tokenAmountToSell / tokensPerEth;
-    uint256 ownerETHBalance = address(this).balance;
-    require(ownerETHBalance >= amountOfETHToTransfer, "Vendor has not enough funds to accept the sell request");
+      // 檢查合約的餘額是否足夠
+      uint256 amountOfETHToTransfer = tokenAmountToSell / tokensPerEth;
+      uint256 ownerETHBalance = address(this).balance;
+      require(ownerETHBalance >= amountOfETHToTransfer, "Vendor has not enough funds to accept the sell request");
 
-    (bool sent) = token.transferFrom(msg.sender, address(this), tokenAmountToSell);
-    require(sent, "Failed to transfer tokens from user to vendor");
+      (bool sent) = token.transferFrom(msg.sender, address(this), tokenAmountToSell);
+      require(sent, "Failed to transfer tokens from user to vendor");
 
 
-    (sent,) = msg.sender.call{value: amountOfETHToTransfer}("");
-    require(sent, "Failed to send ETH to the user");
+      (sent,) = msg.sender.call{value: amountOfETHToTransfer}("");
+      require(sent, "Failed to send ETH to the user");
   }
 }
